d9b8fd025b8915e13312f505e7c6b294edd20585
Inthenew <38168276+Inthenew@users.noreply.github.com>
2021-12-30 17:50:26 -0500

    Update TopBar.js

--- src/TopBar.js
+++ src/TopBar.js
@@ -14,7 +14,7 @@ class TopBar extends Component {
     pop(e) {
         if (!window.vis) {
             window.vis = true;
-            document.getElementById('sidebarloc').innerHTML = `<div id="sidebar"><a id="s1" href="/#" class="s btn btn-outline-success">Home</a><br/><br/><a id="s2" href="/#/signup" class="s btn btn-outline-success">Sign Up</a><br/><br/><a id="s3" href="/#/login" class="s btn btn-outline-success">login</a><br/><br/> <a id="s3" href="/#/logout" class="s btn btn-outline-success">Log Out</a><br/><br/> <a id="s4" href="/#/create" class="s btn btn-outline-success">Create</a><br/><br/></div>`;
+            document.getElementById('sidebarloc').innerHTML = `<div id="sidebar"><a id="s1" href="/#" class="s btn btn-outline-success">Home</a><br/><br/><a id="s2" href="/#/signup" class="s btn btn-outline-success">Sign Up</a><br/><br/><a id="s3" href="/#/login" class="s btn btn-outline-success">login</a><br/><br/> <a id="s3" href="/#/logout" class="s btn btn-outline-success">Log Out</a><br/><br/> <a id="s4" href="/#/create" class="s btn btn-outline-success">Create</a><br/><br/><a id="s5" href="/#/how2use" class="s btn btn-outline-success">Docs</a><br/><br/></div>`;
             $('#fakesidebar').css({
                 visibility: 'visible'
             })
@@ -28,6 +28,19 @@ class TopBar extends Component {
             }
         }
     }
+    onkeyDown(e) {
+        /*
+        if (e.key === 'Enter') {
+            let search = document.getElementById('search');
+            let text = search.value;
+            setTimeout(function () {
+                location.reload();
+            }, 200)
+            window.location.pathname = `/#/search/${text}`;
+        }
+        */
+
+    }
     render() {
         window.vis = false;
         if (localStorage.getItem('Username') !== null) {
@@ -36,7 +49,7 @@ class TopBar extends Component {
                     <ul id="nav">
                         <br/>
                         &nbsp;&nbsp;&nbsp;&nbsp;<Button variant="outline-success" onClick={this.pop} id="options"><img width="40" height="40" src="https://cdn.glitch.me/bf9f93cf-3c4b-42bf-871f-45289c6ff471/more.png?v=1640205361426"/></Button>&nbsp;&nbsp;&nbsp;
-                        <FormControl style={{width: '30%', selfAlign: 'center', display: 'inline-block',   position: 'absolute', left: '35%'}} type="text" placeholder="Search" className="" id="search"/>
+                        <FormControl style={{width: '30%', selfAlign: 'center', display: 'inline-block',   position: 'absolute', left: '35%'}} type="text" onKeyDown={this.onkeyDown} placeholder="Search" className="" id="search"/>
                         <br/>
                         <br/>
                     </ul>
@@ -54,11 +67,11 @@ class TopBar extends Component {
                     <ul id="nav">
                         <br/>
                         &nbsp;&nbsp;&nbsp;&nbsp;<Button variant="outline-success" onClick={this.pop} id="options"><img width="40" height="40" src="https://cdn.glitch.me/bf9f93cf-3c4b-42bf-871f-45289c6ff471/more.png?v=1640205361426"/></Button>&nbsp;&nbsp;&nbsp;
-                        <FormControl style={{width: '30%', selfAlign: 'center', display: 'inline-block',   position: 'absolute', left: '35%'}} type="text" placeholder="Search" className="" id="search"/>
+                        <FormControl style={{width: '30%', selfAlign: 'center', display: 'inline-block',   position: 'absolute', left: '35%'}} type="text" onKeyDown={this.onkeyDown} placeholder="Search" className="" id="search"/>
                         <br/>
                         <br/>
                         <div id="info23">
-                        <Button variant="outline-success" href="/#/login/" style={{ position: 'absolute', right: '30px', top: '30px' }} id="Login2">Login</Button>
+                            <Button variant="outline-success" href="/#/login/" style={{ position: 'absolute', right: '30px', top: '30px' }} id="Login2">Login</Button>
                         </div>
                     </ul>
                     <div id="sidebarloc">
